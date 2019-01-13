717b35e5d6dbcf33860e69a1d2f44cf206608de2
Julio M. Fernandez <jfernandez74@gmail.com>
2019-01-13 14:40:40 +0100

    Section 7 completed

--- pages/views.py
+++ pages/views.py
@@ -1,9 +1,27 @@
 from django.shortcuts import render
+from listings.models import Listing
+from realtors.models import Realtor
+from listings.choices import state_choices, bedroom_choices, price_choices
 
 
 def index(request):
-    return render(request, 'pages/index.html')
+    listings = Listing.objects.order_by('-list_date').filter(is_published=True)[:3]
+
+    data = {
+        'listings': listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+    }
+    return render(request, 'pages/index.html', data)
 
 
 def about(request):
-    return render(request, 'pages/about.html')
+    realtors = Realtor.objects.order_by('-hire_date')
+    mvp_realtors = Realtor.objects.all().filter(is_mvp=True)[:1]
+
+    data = {
+        'realtors': realtors,
+        'mvp_realtors': mvp_realtors
+    }
+    return render(request, 'pages/about.html', data)
